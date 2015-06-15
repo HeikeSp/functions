319c790e00d36f118ee45606aa9cf8e062ac72d6
Heike Sprenger <HeikeSp@yahoo.com>
2015-06-15 18:08:00 +0200

    add another function to relevel treatment and cultivar

--- func_relevel_factors.R
+++ func_relevel_factors.R
@@ -37,3 +37,33 @@ func_relevel_factors <- function(trial_matrix,
   
   return(trial_matrix)
 }
+
+
+#############################################################
+
+# Relevel cultivar and treatment
+func_relevel_factors_2 <- function(trial_matrix,
+                                 treatment_levels=c("control", "drought stress"),
+                                 cultivar_levels=c("Alegria", "Desiree", "Milva", "Saturna"),
+                                 cultivar_levels_sorted=c("Alegria", "Milva", "Desiree", "Saturna"),
+                                 sample_time_levels=c("early/before","early/after", "late/before", "late/after")) {
+  
+  # drop unused levels
+  trial_matrix$treatment <- droplevels(trial_matrix$treatment)
+  trial_matrix$cultivar <- droplevels(trial_matrix$cultivar)
+  
+  # rename levels of treatment factor
+  # http://www.cookbook-r.com/Manipulating_data/Renaming_levels_of_a_factor/
+  levels(trial_matrix$treatment) <- treatment_levels
+  
+  # cultivar needs to be renamed AND reordered!
+  levels(trial_matrix$cultivar) <- cultivar_levels
+  trial_matrix$cultivar <- factor(trial_matrix$cultivar, levels=cultivar_levels_sorted)
+  
+  print("levels of treatment after relevel:")
+  print(levels(trial_matrix$treatment))
+  print("levels of cultivar after relevel:")
+  print(levels(trial_matrix$cultivar))
+  
+  return(trial_matrix)
+}
