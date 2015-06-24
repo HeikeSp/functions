49613792fd2eb56167177110dfaf7d25a3587c70
heikesp <heikesp@yahoo.com>
2015-06-24 20:44:55 +0200

    added function to load standard packages and functions
    added function to load standard packages and functions for metabolite
    data analysis

---
+++ func_load_packages_metabolite_data_analysis.R
@@ -0,0 +1,12 @@
+## load standard packages for metabolite data analysis
+
+library(knitr)
+library(ggplot2)
+library(reshape)
+library(reshape2)
+library(pander)
+library(plyr)
+library(pcaMethods)
+library(psych)
+library(corrgram)
+library(corrplot)
