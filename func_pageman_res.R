985e00782cca5bd3d48f5a00dacaf8a1fac208aa
heikesp <heikesp@yahoo.com>
2015-08-17 14:11:13 +0200

    minor change

--- func_pageman_res.R
+++ func_pageman_res.R
@@ -1,11 +1,18 @@
 # function to get pageman table
 
 func_pageman_res <- function(res){
+  # create dir for up/down-regulation
   res_dir <- rep(0, nrow(res$merge))
+  
   res_dir[res$merge$logFC > 0 & res$merge$FDR < 0.05] <- 1
   res_dir[res$merge$logFC < 0 & res$merge$FDR < 0.05] <- -1
+  
+  # column 8: pgsc_dmp
+  # column 2: logFC
   res_pageman <- cbind(res$merge[,c(8,2)], dir = res_dir)
+  
   #print(head(res_pageman))
   #print(table(res_dir))
+  
   return(res_pageman)
 }
