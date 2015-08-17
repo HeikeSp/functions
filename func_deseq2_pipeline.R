7f3f9c97e485d4ce0638d806d6dec4a6dedb19da
heikesp <heikesp@yahoo.com>
2015-08-17 14:04:37 +0200

    changed MA-plot and PCA plot

--- func_deseq2_pipeline.R
+++ func_deseq2_pipeline.R
@@ -61,7 +61,7 @@ func_deseq2_pipeline_trial <- function(dds_object,  val1){
 func_deseq2_plots <- function(filename, res){
   pdf(filename, width=6, height=6)
   
-  plotMA(res[[2]], ylim=c(-5, 5))
+  DESeq2::plotMA(res[[2]], ylim=c(-5, 5))
   plotDispEsts( res[[1]])
   hist( res[[2]]$pvalue, breaks=20, col="grey" )
   
@@ -76,7 +76,8 @@ func_deseq2_plots <- function(filename, res){
             rgb(0, 0, ramp),
             rgb(ramp, 0, ramp),
             rgb(0, ramp, 0))
-  print( plotPCA( res[[6]], intgroup = c( "cultivar", "condition"), col=cols ) )
+  print( plotPCA( res[[6]], intgroup = c( "cultivar", "condition") ) )
+  # print( plotPCA( res[[6]], intgroup = c( "cultivar", "condition"), col=cols ) )
   
   print( heatmap.2( assay(res[[6]])[ res[[8]], ], scale="row",
                     trace="none", dendrogram="both",
