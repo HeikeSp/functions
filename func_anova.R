2088e951944c1602e16df05bfc9cd886c4bc803d
Heike Sprenger <heikesp@yahoo.com>
2015-11-18 23:38:45 +0100

    added function for ANOVA with two factors (without interaction)

--- func_anova.R
+++ func_anova.R
@@ -7,7 +7,7 @@
 
 # using 3 factors (without interaction)
 
-func_anova_3fac <- function(normalized_values, trial_factors, factor1, factor2, factor3, threshold, analytes=analytes_6sel_exp_sort){
+func_anova_3fac <- function(normalized_values, trial_factors, factor1, factor2, factor3, threshold, analyte_names=analytes_sel_exp_sort$Name){
   res_anova <- matrix("NA", ncol=4, nrow=ncol(normalized_values))
   for (i in 1:ncol(normalized_values)){
     res_anova[i,] <- summary(aov(normalized_values[,i] ~ 
@@ -39,7 +39,47 @@ func_anova_3fac <- function(normalized_values, trial_factors, factor1, factor2, 
 #       if(ncol(normalized_values)==108) # --> jki feld 2012
 #         rownames(res_anova_adj) <- analytes$name[-c(99,65)]
 #     else  
-  rownames(res_anova_adj) <- analytes$name
+  rownames(res_anova_adj) <- analyte_names
+  
+  print(head(res_anova_adj))
+  return(res_anova_adj)
+}
+
+#######################################################################
+
+# using 2 factors (without interaction)
+
+func_anova_2fac <- function(normalized_values, trial_factors, factor1, factor2, threshold, analyte_names=analytes_sel_exp_sort$Name){
+  res_anova <- matrix("NA", ncol = 3, nrow = ncol(normalized_values))
+  for (i in 1:ncol(normalized_values)){
+    res_anova[i,] <- summary(aov(normalized_values[,i] ~ 
+                                   trial_factors[,factor1] + trial_factors[,factor2]))[[1]][["Pr(>F)"]] 
+  }
+  res_anova <- res_anova[,-3]
+  colnames(res_anova) <- c(factor1, factor2)
+  res_anova <- apply(res_anova, 2, as.numeric)
+  
+  res_anova_adj <- apply(res_anova, 2, p.adjust, method="BH")
+  res_anova_adj <- signif(res_anova_adj, 3)  
+  
+  print("dimensions of ANOVA results table:")
+  print(dim(res_anova_adj))
+  
+  print(paste("number of analytes with signifcant effect for", factor1, ", threshold:", threshold))
+  print(length(which(res_anova_adj[,1] < threshold)))
+  print(paste("number of analytes with signifcant effect for", factor2, ", threshold:", threshold))
+  print(length(which(res_anova_adj[,2] < threshold)))
+
+  #   if(ncol(normalized_values)==109) # --> jki test1
+  #     rownames(res_anova_adj) <- analytes$name[-65]
+  #   else
+  #     if(ncol(normalized_values)==106) # --> mpi feld 2012
+  #       rownames(res_anova_adj) <- analytes$name[-c(32,26,73,53)]
+  #     else
+  #       if(ncol(normalized_values)==108) # --> jki feld 2012
+  #         rownames(res_anova_adj) <- analytes$name[-c(99,65)]
+  #     else  
+  rownames(res_anova_adj) <- analyte_names
   
   print(head(res_anova_adj))
   return(res_anova_adj)
@@ -49,7 +89,7 @@ func_anova_3fac <- function(normalized_values, trial_factors, factor1, factor2, 
 
 # using 2 factors (with interaction)
 
-func_anova_2fac_ia <- function(normalized_values, trial_factors, factor1, factor2, threshold, analytes=analytes_6sel_exp_sort){
+func_anova_2fac_ia <- function(normalized_values, trial_factors, factor1, factor2, threshold, analyte_names=analytes_sel_exp_sort$Name){
   res_anova <- matrix("NA", ncol=4, nrow=ncol(normalized_values))
   for (i in 1:ncol(normalized_values)){
     res_anova[i,] <- summary(aov(normalized_values[,i] ~ 
@@ -81,7 +121,7 @@ func_anova_2fac_ia <- function(normalized_values, trial_factors, factor1, factor
 #     if(ncol(normalized_values)==108) # --> jki feld 2012
 #       rownames(res_anova_adj) <- analytes$name[-c(99,65)]
 #   else
-  rownames(res_anova_adj) <- analytes$name
+  rownames(res_anova_adj) <- analyte_names
   
   print(head(res_anova_adj))
   return(res_anova_adj)
@@ -92,7 +132,7 @@ func_anova_2fac_ia <- function(normalized_values, trial_factors, factor1, factor
 
 # using 1 factor
 
-func_anova_1fac <- function(normalized_values, trial_factors, factor1, threshold, analytes=analytes_sel_exp_sort){
+func_anova_1fac <- function(normalized_values, trial_factors, factor1, threshold, analyte_names=analytes_sel_exp_sort$Name){
   res_anova <- matrix("NA", ncol=2, nrow=ncol(normalized_values))
   for (i in 1:ncol(normalized_values)){
     res_anova[i,] <- summary(aov(normalized_values[,i] ~ trial_factors[,factor1]))[[1]][["Pr(>F)"]] 
