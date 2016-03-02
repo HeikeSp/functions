d8d4b35d9829b978d4a236ea1cf03e59309c0d43
Heike Sprenger <HeikeSp@yahoo.com>
2016-03-02 11:25:04 +0100

    do not plot pie charts

--- colors.R
+++ colors.R
@@ -16,7 +16,7 @@ par(mfrow=c(3,2))
 
 heike_palette_7 = c("#04A5CA", "#BF5300", "#7570B3", "#BB417F", "#66A61E", "#E6AB02", "#265298")
 # tuerkis,   orange,   lila,     pink,     gruen,     gelb,     dunkelblau
-pie(rep(1,7),c("#04A5CA", "#BF5300", "#7570B3", "#BB417F", "#66A61E", "#E6AB02", "#265298"), col=heike_palette_7, main="7 colors")
+#pie(rep(1,7),c("#04A5CA", "#BF5300", "#7570B3", "#BB417F", "#66A61E", "#E6AB02", "#265298"), col=heike_palette_7, main="7 colors")
 
 heike_palette_6 = c("#04A5CA", "#BF5300", "#7570B3", "#BB417F", "#66A61E", "#E6AB02")
 # tuerkis,   orange,   lila,     pink,     gruen,     gelb
@@ -38,15 +38,15 @@ heike_palette_2 = c("#BF5300", "#265298")
 #pie(rep(1,2),c("#BF5300", "#265298"), col=c("#BF5300", "#265298"))
 
 cols_treatment <- c("#4F81BD", "#B94441") # e.g. control / stress
-pie(rep(1,2), cols_treatment, col=cols_treatment, main="cols_treatment")
+#pie(rep(1,2), cols_treatment, col=cols_treatment, main="cols_treatment")
 
 cols_sample_time2 <- c("#66A61E", "#7570B3") # 2 time points
 #                       gruen        lila
-pie(rep(1,2), cols_sample_time2, col=cols_sample_time2, main="cols_sample_time")
+#pie(rep(1,2), cols_sample_time2, col=cols_sample_time2, main="cols_sample_time")
 
 cols_sample_time <- c("#74A0D4", "#155096", "#E27572","#6B0705") # 4 time points
 #                       hellblau  dunkelblau hellrot  dunkelrot
-pie(rep(1,4), cols_sample_time, col=cols_sample_time, main="cols_sample_time")
+#pie(rep(1,4), cols_sample_time, col=cols_sample_time, main="cols_sample_time")
 
 cols_treatment_sample_time <- c("#74A0D4", "#E27572", "#155096", "#6B0705") # 4 conditions
 #                             hellblau    hellrot   dunkelblau  dunkelrot
@@ -61,7 +61,7 @@ cols_late <- c("#E27572","#6B0705") # 2 time points (late/before - late/after)
 #pie(1:4, cols_4B, col=cols_4B)
 
 cols_cultivar_check <- c("forestgreen", "darkorange3","darkblue", "deeppink4") # 4 cultivars
-pie(rep(1,4), cols_cultivar_check, col=cols_cultivar_check, main="cols_cultivar")
+#pie(rep(1,4), cols_cultivar_check, col=cols_cultivar_check, main="cols_cultivar")
 
 cols_cultivar2 <- c("#68C468", "#FFA54D","#3D3DF7", "#BA3E82") # 4 cultivars
 
@@ -70,9 +70,10 @@ cols_cultivar_34 <- rainbow(34) # 34 cultivars
 cols_lines_63 <- rainbow(63) # 63 lines
 
 cols_lines_2014 <- rainbow(193) # 193 lines
+cols_crossing <- brewer.pal(3, "Dark2")  # AR, EA, parents
 
 cols_cultivar_treatment <- c("forestgreen","#68C468", "darkorange3","#FFA54D", "darkblue","#3D3DF7", "deeppink4", "#BA3E82") # 4 cultivars / 2 treatments
-pie(rep(1,8), cols_cultivar_treatment, col=cols_cultivar_treatment, main="cols_cultivar_treatment")
+#pie(rep(1,8), cols_cultivar_treatment, col=cols_cultivar_treatment, main="cols_cultivar_treatment")
 par(mfrow=c(1,1))
 
 # convert R color names to HEX code
@@ -87,11 +88,11 @@ col2hex("deeppink4")
 
 cols_treatment_tolerance <- brewer.pal(4, "Paired")
 
-display.brewer.pal(n = 6, name = "Greens")
-display.brewer.pal(n = 6, name = "Oranges")
+#display.brewer.pal(n = 6, name = "Greens")
+#display.brewer.pal(n = 6, name = "Oranges")
 
 cols_trial <- c (brewer.pal(n = 6, name = "Greens")[4:6], brewer.pal(n = 6, name = "Oranges")[4:6])
-pie(rep(1,6), cols_trial, col=cols_trial, main="cols_trial")
+#pie(rep(1,6), cols_trial, col=cols_trial, main="cols_trial")
 # Greens: "#74C476" "#31A354" "#006D2C" --> for field
 # Oranges: "#FD8D3C" "#E6550D" "#A63603" --> for greenhouse
 # cols_trial <- c("#74C476", "#FD8D3C", "#E6550D", "#A63603", "#31A354", "#006D2C")
