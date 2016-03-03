5c188278eb04dd4b5ca54d93b2ecf89236a9d33f
Heike Sprenger <HeikeSp@yahoo.com>
2016-03-03 10:19:27 +0100

    added option to calculate SSI for all replicates per line or median

--- func_calc_ssi.R
+++ func_calc_ssi.R
@@ -1,6 +1,6 @@
 # function to calculate Stress Sensitivity Index (SSI)
 
-func_calc_ssi <- function(yield_trial, plant_lines, si_value){
+func_calc_ssi <- function(yield_trial, plant_lines, si_value, keep_replicates = "yes"){
   
   # calculate relative Starch yield (relSY) per line for each replicate
   relSY <- func_calc_relSY_valdis(yield_trial, plant_lines)
@@ -11,7 +11,11 @@ func_calc_ssi <- function(yield_trial, plant_lines, si_value){
   # calculate 
   ssi <- list()
   for (plant_line in plant_lines){
-    ssi[[plant_line]] <- (1 - unique(relSY_median[[plant_line]]) ) / si_value
+    if(keep_replicates == "yes"){
+      ssi[[plant_line]] <- (1 - unique(relSY[[plant_line]]) ) / si_value # use all replicates per line
+    } else{
+      ssi[[plant_line]] <- (1 - unique(relSY_median[[plant_line]]) ) / si_value # use median per line
+    }
   }
   
   return(ssi)
