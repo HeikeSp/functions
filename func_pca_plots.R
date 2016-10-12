188cf5e8e1661962e3777a10dcc5373b067edfe1
Heike Sprenger <HeikeSp@yahoo.com>
2016-10-12 17:47:30 +0200

    improved code and added func_pca_plot_sym_bg

--- func_pca_plots.R
+++ func_pca_plots.R
@@ -6,25 +6,28 @@
 #===============================================================================
 
 
-func_5pairs_plot <- function(pca_res, factors, color_factor, symbol=19, maintext=""){
+func_5pairs_plot <- function(pca_res, factors, color_factor, symbol = 19, maintext = ""){
   pairs(pca_res@scores[,1:5], 
         col = factors[,color_factor],
         pch=symbol, 
         main=maintext)
 }
 
-func_5pairs_plot_sym <- function(pca_res, factors, color_factor, symbols=c(17,19), symbol_factor, maintext=""){
+func_5pairs_plot_sym <- function(pca_res, factors, color_factor, 
+                                 symbols = c(17,19), symbol_factor, maintext = ""){
   pairs(pca_res@scores[,1:5], 
         col = factors[,color_factor],
         pch = symbols[as.integer(factors[,symbol_factor])], 
         main=maintext)
-  
-}
+  }
 
 
-func_pca_plot <- function(pca_res, dim1, dim2, factors, color_factor, symbols=19, symbol_size=1.5, pos1, leg1, 
-                          maintext="", legend.text, xmin=min(pca_res@scores[,dim1], na.rm=T), xmax=max(pca_res@scores[,dim1], na.rm=T),
-                          ymin=min(pca_res@scores[,dim2], na.rm=T), ymax=max(pca_res@scores[,dim2], na.rm=T))
+func_pca_plot <- function(pca_res, dim1, dim2, factors, color_factor, symbols = 19, 
+                          symbol_size = 1.5, pos1, leg1, maintext = "", legend.text, 
+                          xmin = min(pca_res@scores[,dim1], na.rm = T), 
+                          xmax = max(pca_res@scores[,dim1], na.rm = T),
+                          ymin = min(pca_res@scores[,dim2], na.rm = T), 
+                          ymax = max(pca_res@scores[,dim2], na.rm = T))
 {
   plot(pca_res@scores[,dim1], pca_res@scores[,dim2], 
        xlab = paste("PC",dim1, " (", round(pca_res@R2[dim1]*100, 1),"%)", sep=""),
@@ -45,10 +48,13 @@ func_pca_plot <- function(pca_res, dim1, dim2, factors, color_factor, symbols=19
          legend=legend.text)
 }
 
-func_pca_plot_sym <- function(pca_res, dim1, dim2, factors, color_factor, symbols=c(17,19), 
-                              symbol_factor, symbol_size=1.5, pos1, leg1, pos2, leg2, maintext="",
-                              legend.text1, legend.text2, xmin=min(pca_res@scores[,dim1], na.rm=T), xmax=max(pca_res@scores[,dim1], na.rm=T),
-                              ymin=min(pca_res@scores[,dim2], na.rm=T), ymax=max(pca_res@scores[,dim2], na.rm=T))
+func_pca_plot_sym <- function(pca_res, dim1, dim2, factors, color_factor, symbols = c(17,19), 
+                              symbol_factor, symbol_size = 1.5, pos1, leg1, pos2, leg2, 
+                              maintext = "", legend.text1, legend.text2, 
+                              xmin = min(pca_res@scores[,dim1], na.rm = T), 
+                              xmax = max(pca_res@scores[,dim1], na.rm = T),
+                              ymin = min(pca_res@scores[,dim2], na.rm = T), 
+                              ymax = max(pca_res@scores[,dim2], na.rm = T))
 {
   plot(pca_res@scores[,dim1], pca_res@scores[,dim2], 
        xlab = paste("PC",dim1, " (", round(pca_res@R2[dim1]*100, 1),"%)", sep=""),
@@ -57,19 +63,54 @@ func_pca_plot_sym <- function(pca_res, dim1, dim2, factors, color_factor, symbol
        pch = symbols[as.integer(factors[,symbol_factor])], 
        main = maintext, 
        cex = symbol_size,
-       cex.lab=1.4, 
-       cex.axis=1.2,
-       xlim=c(xmin, xmax),
-       ylim=c(ymin, ymax)
+       cex.lab = 1.4, 
+       cex.axis = 1.2,
+       xlim = c(xmin, xmax),
+       ylim = c(ymin, ymax)
   )
   
-  legend(pos1, cex=leg1, bty="n",
+  legend(pos1, cex = leg1, bty="n",
          levels(factors[,color_factor]), 
-         fill=1:length(levels(factors[,color_factor])),
-         legend=legend.text1)
+         fill = 1:length(levels(factors[,color_factor])),
+         legend = legend.text1)
   
-  legend(pos2, cex=leg2, bty="n",
+  legend(pos2, cex = leg2, bty="n",
          levels(factors[,symbol_factor]), 
-         pch=symbols,
-         legend=legend.text2)
+         pch = symbols,
+         legend = legend.text2)
+}
+
+
+func_pca_plot_sym_bg <- function(pca_res, dim1, dim2, factors, color_factor, symbols = c(21,22), 
+                              symbol_factor, symbol_size = 1.5, pos1, leg1, pos2, leg2, 
+                              maintext = "", legend.text1, legend.text2, 
+                              xmin = min(pca_res@scores[,dim1], na.rm = T), 
+                              xmax = max(pca_res@scores[,dim1], na.rm = T),
+                              ymin = min(pca_res@scores[,dim2], na.rm = T), 
+                              ymax = max(pca_res@scores[,dim2], na.rm = T),
+                              border_col = "black")
+{
+  plot(pca_res@scores[,dim1], pca_res@scores[,dim2], 
+       xlab = paste("PC",dim1, " (", round(pca_res@R2[dim1]*100, 1),"%)", sep=""),
+       ylab = paste("PC",dim2, " (", round(pca_res@R2[dim2]*100, 1),"%)", sep=""),
+       bg = factors[,color_factor], # background color
+       col = border_col, # border color
+       pch = symbols[as.integer(factors[,symbol_factor])], 
+       main = maintext, 
+       cex = symbol_size,
+       cex.lab = 1.4, 
+       cex.axis = 1.2,
+       xlim = c(xmin, xmax),
+       ylim = c(ymin, ymax)
+  )
+  
+  legend(pos1, cex = leg1, bty = "n",
+         levels(factors[,color_factor]), 
+         fill = 1:length(levels(factors[,color_factor])),
+         legend = legend.text1)
+  
+  legend(pos2, cex = leg2, bty = "n",
+         levels(factors[,symbol_factor]), 
+         pch = symbols,
+         legend = legend.text2)
 }
