951d01b95b3c1c8dfcda6960b33e3b3f7f8b65e0
hespr <heikesp@yahoo.com>
2018-02-13 11:12:13 +0100

    Added function for outlier identification per replicate group and metabolite

--- func_find_one_outlier.R
+++ func_find_one_outlier.R
@@ -7,35 +7,41 @@ library(outliers)
 
 find_one_outlier <- function (x, output="none", opposite = FALSE)
 {
-  DNAME <- deparse(substitute(x))
-  x <- sort(x[complete.cases(x)])
-  n <- length(x)
-  
-  if (xor(((x[n] - mean(x)) < (mean(x) - x[1])), opposite)) {
-    alt = paste("lowest value", x[1], "is an outlier")
-    o <- x[1]
-    d <- x[2:n]
-  }
-  else {
-    alt = paste("highest value", x[n], "is an outlier")
-    o <- x[n]
-    d <- x[1:(n - 1)]
-  }
-  g <- abs(o - mean(x))/sd(x)
-  u <- var(d)/var(x) * (n - 2)/(n - 1)
-  pval <- 1 - pgrubbs(g, n, type = 10)
-  method <- "Grubbs test for one outlier"
-  
-  RVAL <- list(statistic = c(G = g, U = u), alternative = alt,
-               p.value = pval, method = method, data.name = DNAME,
-               outlier_value = o, outlier_rowname = g)
-  class(RVAL) <- "htest"
-  if (output=="name")
-    return(names(RVAL$outlier_value))
-  else if (output=="p.value")
-    return(RVAL$p.value)
-  else
+  if(sum(is.na(x)) == length(x)){
+    RVAL <- NA
     return(RVAL)
+  } else {
+  
+    DNAME <- deparse(substitute(x))
+    x <- sort(x[complete.cases(x)])
+    n <- length(x)
+    
+    if (xor(((x[n] - mean(x)) < (mean(x) - x[1])), opposite)) {
+      alt = paste("lowest value", x[1], "is an outlier")
+      o <- x[1]
+      d <- x[2:n]
+    }
+    else {
+      alt = paste("highest value", x[n], "is an outlier")
+      o <- x[n]
+      d <- x[1:(n - 1)]
+    }
+    g <- abs(o - mean(x))/sd(x)
+    u <- var(d)/var(x) * (n - 2)/(n - 1)
+    pval <- 1 - pgrubbs(g, n, type = 10)
+    method <- "Grubbs test for one outlier"
+    
+    RVAL <- list(statistic = c(G = g, U = u), alternative = alt,
+                 p.value = pval, method = method, data.name = DNAME,
+                 outlier_value = o, outlier_rowname = g)
+    class(RVAL) <- "htest"
+    if (output=="name")
+      return(names(RVAL$outlier_value))
+    else if (output=="p.value")
+      return(as.numeric(RVAL$p.value))
+    else
+      return(RVAL)
+  }
 }
 
 
--- func_replace_outlier.R
+++ func_replace_outlier.R
@@ -1,3 +1,8 @@
+# is applied to all values per metabolite (iterative over all metabolites)
+# will be continued as long the p-value of grubbs test is below threshold
+# outlier is replaced by NA
+# values matrix contains metbolites in columns, samples in rows
+
 func_replace_outlier <- function(values, threshold, original_values, output="original"){
   idx_outlier_col = 1
   
@@ -28,4 +33,61 @@ func_replace_outlier <- function(values, threshold, original_values, output="ori
     return(original_values)
   else 
     return(values)
+}
+
+
+func_replace_outlier2 <- function(data, threshold){
+  
+  check <- 1
+  grp_vals <- levels(data$timing_tissue_treatment)
+  
+  while(check > 0){
+    
+    outlier_name <- matrix(NA, nrow = length(grp_vals), ncol = ncol(data))
+    outlier_pvalue <- matrix(NA, nrow = length(grp_vals), ncol = ncol(data))
+    
+    for(j in 1:length(grp_vals)){
+      
+      val <- grp_vals[j]
+      data_sub <- droplevels(subset(data, timing_tissue_treatment == val))
+    
+      for (i in 2:ncol(data_sub)){
+        x <- data_sub[,i]
+        names(x) <- rownames(data_sub)
+        
+        outlier_pvalue[j,i] <- find_one_outlier(x, output = "p.value")
+        outlier_name[j,i] <- find_one_outlier(x, output = "name")
+        }
+      }
+    
+    outlier_name[,1] <- outlier_pvalue[,1] <- grp_vals
+    colnames(outlier_name) <- colnames(outlier_pvalue) <- colnames(data)
+    
+    outlier_pvalue <- data.frame(timing_tissue_treatment = outlier_pvalue[,1],
+                                 apply(outlier_pvalue[,-1], 2, as.numeric), check.names = F)
+    
+    # melt grubbs test results
+    outlier_pvalue_m <- melt(outlier_pvalue)
+    outlier_name_m <- melt(data.frame(outlier_name, check.names = F), id = "timing_tissue_treatment")
+
+    outlier_pvalue_name <- merge(outlier_name_m, outlier_pvalue_m,
+                                 by = c("timing_tissue_treatment", "variable"))
+    colnames(outlier_pvalue_name)[3:4] <- c("sampleID","p_value")
+    outlier_pvalue_name$p_adj <- p.adjust(outlier_pvalue_name$p_value, method = "BH")
+    
+    outlier_pvalue_name_sig <- droplevels(subset(outlier_pvalue_name, p_adj < threshold))
+    
+    check <- nrow(outlier_pvalue_name_sig)
+
+    # replace outliers by NA
+    if(check > 0){
+      for(z in 1:check){
+        a <- as.character(outlier_pvalue_name_sig$sampleID[z]) # row
+        b <- as.character(outlier_pvalue_name_sig$variable[z]) # column
+        data[a,b] <- NA
+      }
+    }
+  }
+  
+  return(data)
 }
